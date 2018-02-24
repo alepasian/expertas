0aa2945aff64da10c0478c5d74f33069c0b7de0f
alepasian <alejandra@semexpert.com.ar>
2018-02-24 09:53:48 -0300

    wordpress de 0 con login y perfiles de usuario resuelto registracion completa y mi perfil incompleto

--- backwpup_readme.txt
+++
@@ -1,3 +0,0 @@
-Usted puede haber notado el archivo manifest.json en este archivo.
-manifest.json podría ser necesaria para su posterior restauración de una copia de seguridad de este archivo.
-Por favor deje sin tocar manifest.json y en su sitio. De otro modo la seguridad será ignorada.
--- wp-config.php
+++ wp-config.php
@@ -46,26 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         's0Cg0nuJn8Le2RnupZKV7rwIOH4aHxc0gOXHHsqdIGLKoHWKXrEdV4kxpO0y9k4k');
-define('SECURE_AUTH_KEY',  'VpZW7gzh9gASNNG8df9P1yPxaRltKazHUWZPnOLSFpFIcy7hOTGc20vieDAGshfQ');
-define('LOGGED_IN_KEY',    'PhEfleogUKhG2IiCeaDWYEAxelKS5SxgnJdMSbxHs6Lar0uTB0LcSp1QxFuhQjBZ');
-define('NONCE_KEY',        'V27jMLwraFEcbInAQtUD6MLWqPvq4DYen82LjLAFcuAMl81UEqYEQx24drrUofSR');
-define('AUTH_SALT',        'BISoN6PA9i6sSeEdQvyRDWg1ipM4WmNyEGRpbwnnax6htu5blYd2kbUm1dtCX0ub');
-define('SECURE_AUTH_SALT', 'M5RtACY5tQoqbmpsT6LSpR9UcW0OzSgfWQDquW6xEvRzNkbLTnKpfG5NBBO7LeMm');
-define('LOGGED_IN_SALT',   'eECA0l0qlsvkPd3dZbDrolNSQmAWRCXQ6YAAvmCI1PIPSmMDn6cCWY3M2PgkYhYV');
-define('NONCE_SALT',       'OQjM2rTcXmJWRz25eVsMKAF4hquphLmg6IUelGP8KiFtgFcz2iIrkuE5611mKpAY');
-
-/**
- * Other customizations.
- */
-define('FS_METHOD','direct');define('FS_CHMOD_DIR',0755);define('FS_CHMOD_FILE',0644);
-define('WP_TEMP_DIR',dirname(__FILE__).'/wp-content/uploads');
-
-/**
- * Turn off automatic updates since these are managed upstream.
- */
-define('AUTOMATIC_UPDATER_DISABLED', true);
-
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
@@ -75,7 +63,7 @@ define('AUTOMATIC_UPDATER_DISABLED', true);
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'mvx8_';
+$table_prefix  = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -89,7 +77,7 @@ $table_prefix  = 'mvx8_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
